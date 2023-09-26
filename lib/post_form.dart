e5362c60f217cef3497af7fc99d7b4a53fbe8cd3
dlrlfgh1210 <dlrlfgh517@gmail.com>
2023-09-26 10:48:26 +0900

    PostScreen Refactoring

---
+++ lib/post_form.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class PostForm extends StatefulWidget {
+  const PostForm({super.key});
+
+  @override
+  State<PostForm> createState() => _PostFormState();
+}
+
+class _PostFormState extends State<PostForm> {
+  String _post = "";
+  final TextEditingController _postEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _postEditingController.addListener(() {
+      setState(() {
+        _post = _postEditingController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _postEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10,
+      ),
+      height: 200,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        boxShadow: const [
+          BoxShadow(
+              color: Colors.black,
+              blurRadius: 5,
+              blurStyle: BlurStyle.outer,
+              spreadRadius: 5,
+              offset: Offset(0, 5))
+        ],
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        maxLines: null,
+        minLines: null,
+        controller: _postEditingController,
+        decoration: const InputDecoration(
+            hintText: "Write it down here!", border: InputBorder.none),
+      ),
+    );
+  }
+}
--- lib/post_screen.dart
+++ lib/post_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_semi_final/change_color_button.dart';
 import 'package:flutter_semi_final/mood_container.dart';
+import 'package:flutter_semi_final/post_form.dart';
 
 class PostScreen extends StatefulWidget {
   static const routeName = "Post";
@@ -12,8 +13,6 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
-  String _post = "";
-  final TextEditingController _postEditingController = TextEditingController();
   int selectedMoodIndex = -1;
   void onMoodSelected(int index) {
     setState(() {
@@ -22,35 +21,17 @@ class _PostScreenState extends State<PostScreen> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    _postEditingController.addListener(() {
-      setState(() {
-        _post = _postEditingController.text;
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    _postEditingController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-
     return Scaffold(
       resizeToAvoidBottomInset: true,
       appBar: AppBar(
         title: const Text("🔥MOOD🔥"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 36,
-        ),
-        child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 36,
+          ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -65,33 +46,7 @@ class _PostScreenState extends State<PostScreen> {
               const SizedBox(
                 height: 10,
               ),
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 10,
-                ),
-                height: 200,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  boxShadow: const [
-                    BoxShadow(
-                        color: Colors.black,
-                        blurRadius: 5,
-                        blurStyle: BlurStyle.outer,
-                        spreadRadius: 5,
-                        offset: Offset(0, 5))
-                  ],
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: TextField(
-                  maxLines: null,
-                  minLines: null,
-                  controller: _postEditingController,
-                  decoration: const InputDecoration(
-                      hintText: "Write it down here!",
-                      border: InputBorder.none),
-                ),
-              ),
+              const PostForm(),
               const SizedBox(
                 height: 30,
               ),
